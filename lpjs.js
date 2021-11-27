621b213726ef6016eb401612dd7ea1919e6f3179
yuukikinjou0401 <95122200+yuukikinjou0401@users.noreply.github.com>
2021-11-27 17:59:07 +0900

    Delete lpjs.js

--- lpjs.js
+++
@@ -1,8 +0,0 @@
-function fncBMI(){
-    var bmi,w,h;
-    w=eval(document.bmi.bmiWeight.value);
-    h=eval(document.bmi.bmiHeight.value)/100;
-    bmi=w
-    alert('あなたのBMI値は'+bmi+'です');
-    document.bmi.bmiAns.value=bmi;
-}
